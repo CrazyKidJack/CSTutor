202b7e1ad169e0310968c2eb4ea9abe71b6305d0
Jackson Pfeffer <jackpfeffer@yahoo.com>
2020-10-13 04:28:20 -0500

    -code clean up :)

--- Sources/driver2.cpp
+++ Sources/driver2.cpp
@@ -12,6 +12,7 @@
 
 //UTILITY
 #include <filesystem> //path
+#include <ctype.h>
 
 //CUSTOM
 #include "../Headers/Dictionary/Dictionary.hpp"
@@ -23,36 +24,118 @@ using filesystem::path;
 //////////////////////////////////////////////////////////////////////////////
 // FUNCTION DECLARATIONS
 //////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+//gets a valid file path
+path getDictPath();
+
+//----------------------------------------------------------------------------
+//changes wordTable to reflect all possible words in the phrase
+//pre-condition: wordTable contains a single, empty wordLst
+//                 and the tracking nodePtr for that list ponints
+//                 to the root of the Dictionary
+void parsePhrase(
+  string const &phrase,
+  map<int, pair<DNshPtr, vector<DNshPtr>>> &wordTable
+);
+
+//----------------------------------------------------------------------------
+void prntWordTable(map<int, pair<DNshPtr, vector<DNshPtr>>> const &wordTable);
+
+//----------------------------------------------------------------------------
+//gets a list of possible phrases from the wordTable
 vector<vector<DNshPtr>> getPhrases(
   map<int, pair<DNshPtr, vector<DNshPtr>>> const &wordTable,
   int idx
+);
+
+//----------------------------------------------------------------------------
+//MODIFIES maxPhrase and maxFreq to represet the phrase
+//  with the highest possible cummulative frequency in the phraseLst
+void findBestPhrase(
+  vector<vector<DNshPtr>> const &phraseLst,
+  string &maxPhrase,
+  double &maxFreq
 );
 
 //////////////////////////////////////////////////////////////////////////////
 // MAIN FUNCTION
 //////////////////////////////////////////////////////////////////////////////
 int main(){
+  path dictFilePath(getDictPath());
+
+  Dictionary dict(dictFilePath);
+
+  char response;
+  cout << "Dictionary size= " << dict.getSize() << endl;
+  cout << "Are you sure you want to print the dictionary (y/n)? ";
+  cin  >> response;
+  response = tolower(response);
+
+  if(response == 'y'){
+    cerr << "Dictionary: " << endl
+       << dict << endl
+       << endl;
+  }
+
+  string phrase;
+  cout << "phrase: ";
+  cin  >> phrase;
+
+//initialize wordTable to a single, empty wordLst
+//  the tracking nodePtr for that list points to the
+//  root of the dictionary to prepare for parsing
+  map<int, pair<DNshPtr, vector<DNshPtr>>> wordTable{{
+    0,
+    {
+      DNshPtr(DNshPtr{}, &dict),
+      vector<DNshPtr>()
+    }
+  }};
+
+  parsePhrase(phrase, wordTable);
+
+  erase_if(wordTable, [](auto const &elem) {
+    vector<DNshPtr> const &wordLst = elem.second.second;
+    return wordLst.empty();
+  });
+
+  prntWordTable(wordTable);
+
+  vector<vector<DNshPtr>> phraseLst(getPhrases(wordTable, 0));
+
+  string maxPhrase;
+  double maxFreq;
+  findBestPhrase(phraseLst, maxPhrase, maxFreq);
+
+  cout << "max phrase: " << maxPhrase << endl
+       << "max frequency= " << maxFreq << endl;
+
+  return 0;
+}//end main
+//////////////////////////////////////////////////////////////////////////////
+// END MAIN FUNCTION
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// FUNCTION DEFINITIONS
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+path getDictPath(){
   path dictFilePath;
   cout << "Dictionary file name: ";
   cin  >> dictFilePath;
   if(!exists(dictFilePath))
     cerr << "File" << dictFilePath << " doesn't exist" << endl;
 
-  Dictionary dict(dictFilePath);
+  return dictFilePath;
+}//end getDictPath
 
-  cerr << "Dictionary: " << endl
-       << dict << endl
-       << endl;
-
-  string phrase;
-  cout << "phrase: ";
-  cin  >> phrase;
-
-  map<int, pair<DNshPtr, vector<DNshPtr>>> wordTable;
-  wordTable.emplace(
-    0,
-    make_pair(DNshPtr(DNshPtr{}, &dict), vector<DNshPtr>())
-  );
+//----------------------------------------------------------------------------
+void parsePhrase(
+  string const &phrase,
+  map<int, pair<DNshPtr, vector<DNshPtr>>> &wordTable
+){
+  DNshPtr const dictPtr(wordTable.at(0).first);
 
   //LOOP THRU PARSE PHRASE
   for (int idx = 0; idx < phrase.length(); ++idx){
@@ -75,19 +158,17 @@ int main(){
         val.second.push_back(currNodePtr);
         newWordTable.emplace(
           idx+1,
-          make_pair(DNshPtr(DNshPtr{}, &dict), vector<DNshPtr>())
+          make_pair(DNshPtr(DNshPtr{}, dictPtr.get()), vector<DNshPtr>())
         );
       }//end if currNode is a word
     }//END LOOP THRU wordTable
 
     for(auto& elem : newWordTable) wordTable.insert(move(elem));
   }//END LOOP THRU PARSE PHRASE
+}//end parsePhrase
 
-  erase_if(wordTable, [](auto const &elem) {
-    vector<DNshPtr> const &wordLst = elem.second.second;
-    return wordLst.empty();
-  });
-
+//----------------------------------------------------------------------------
+void prntWordTable(map<int, pair<DNshPtr, vector<DNshPtr>>> const &wordTable){
   for (auto const &[key, val] : wordTable){
     cout << "wordTable[" << key << "] = {";
     for (int idx = 0; idx < val.second.size(); ++idx){
@@ -96,40 +177,9 @@ int main(){
     }
     cout << "}" << endl;
   }
+}//end prntWordTable
 
-  vector<vector<DNshPtr>> phraseLst(getPhrases(wordTable, 0));
-
-  double maxFreq = 0.0;
-  string maxPhrase;
-  double currFreq = 0.0;
-  string currPhrase;
-  for(vector<DNshPtr> const& phrase : phraseLst){
-    for(DNshPtr const ptr : phrase){
-      currFreq += ptr->freq();
-      if(currPhrase.empty()) currPhrase = ptr->lemma();
-      else currPhrase = currPhrase + " " + ptr->lemma();
-    }
-
-    if(currFreq > maxFreq){
-      maxFreq = currFreq;
-      maxPhrase = currPhrase;
-    }
-    currFreq = 0.0;
-    currPhrase = "";
-  }
-
-  cout << "max phrase: " << maxPhrase << endl
-       << "max frequency= " << maxFreq << endl;
-
-  return 0;
-}//end main
-//////////////////////////////////////////////////////////////////////////////
-// END MAIN FUNCTION
-//////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////
-// FUNCTION DEFINITIONS
-//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
 vector<vector<DNshPtr>> getPhrases(
   map<int, pair<DNshPtr, vector<DNshPtr>>> const& wordTable,
   int idx
@@ -150,7 +200,34 @@ vector<vector<DNshPtr>> getPhrases(
   }
 
   return rtn;
-}//end foo
+}//end getPhrases
+
+//----------------------------------------------------------------------------
+void findBestPhrase(
+  vector<vector<DNshPtr>> const &phraseLst,
+  string& maxPhrase,
+  double& maxFreq
+){
+  maxFreq = 0.0;
+  maxPhrase = "";
+  double currFreq = 0.0;
+  string currPhrase;
+  //LOOP THRU phraseLst
+  for(vector<DNshPtr> const& phrase : phraseLst){
+    for(DNshPtr const ptr : phrase){
+      currFreq += ptr->freq();
+      if(currPhrase.empty()) currPhrase = ptr->lemma();
+      else currPhrase = currPhrase + " " + ptr->lemma();
+    }
+
+    if(currFreq > maxFreq){
+      maxFreq = currFreq;
+      maxPhrase = currPhrase;
+    }
+    currFreq = 0.0;
+    currPhrase = "";
+  }//END LOOP THRU phraseLst
+}//end findBestPhrase
 
 
 
